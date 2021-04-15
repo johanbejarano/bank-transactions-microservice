014850273524fd5f5fec287a0aac60c7e9be63d4
Johan Bejarano <jbejarano@vortexbird.com>
2021-04-15 14:33:32 -0500

    Los microservicios se registran y se consumen por Eureka

--- src/main/java/com/vobi/devops/bank/openfeignclients/UsersServiceClient.java
+++ src/main/java/com/vobi/devops/bank/openfeignclients/UsersServiceClient.java
@@ -3,18 +3,24 @@ package com.vobi.devops.bank.openfeignclients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import com.vobi.devops.bank.dto.LoginRequest;
+import com.vobi.devops.bank.dto.LoginResponse;
 import com.vobi.devops.bank.dto.UsersDTO;
 
 @FeignClient(
-		url = "${users.service.url}",
-		value = "users-service-client"
+		value = "users-service"
 		)
 public interface UsersServiceClient {
 
-	@GetMapping("/{userEmail}")
+	@GetMapping("/api/v1/users/{userEmail}")
 	public UsersDTO getUser(
 			@PathVariable("userEmail") String userEmail
 			);
+	
+	@PostMapping("/login")
+	public LoginResponse login (@RequestBody LoginRequest loginRequest);
 	
 }
